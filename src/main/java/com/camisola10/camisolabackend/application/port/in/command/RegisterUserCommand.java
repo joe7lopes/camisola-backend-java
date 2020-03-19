b0d75c2b3b2c0c60fa2fc0eaad346d0a2512641c
Joao Oliveira <j.andre.oliveira@gmail.com>
2020-03-19 20:43:03 +0100

    added self

--- src/main/java/com/camisola10/camisolabackend/application/port/in/command/RegisterUserCommand.java
+++ src/main/java/com/camisola10/camisolabackend/application/port/in/command/RegisterUserCommand.java
@@ -1,10 +1,13 @@
 package com.camisola10.camisolabackend.application.port.in.command;
 
+import lombok.Value;
+
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
-import java.util.Objects;
 
-public class RegisterUserCommand {
+@Value
+public class RegisterUserCommand extends SelfValidating<RegisterUserCommand> {
+
     @NotBlank
     private final String firstName;
     @NotBlank
@@ -19,37 +22,6 @@ public class RegisterUserCommand {
         this.lastName = lastName;
         this.email = email;
         this.password = password;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        RegisterUserCommand that = (RegisterUserCommand) o;
-        return Objects.equals(firstName, that.firstName) &&
-                Objects.equals(lastName, that.lastName) &&
-                Objects.equals(email, that.email) &&
-                Objects.equals(password, that.password);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(firstName, lastName, email, password);
+        validateSelf();
     }
 }
