e442229f19562f87797c6e4b3c9e23b6bbbb5b65
Joao Oliveira <j.andre.oliveira@gmail.com>
2020-05-13 14:24:05 +0200

    added id for Product.

--- src/main/java/com/camisola10/camisolabackend/adapter/persistence/product/ProductDb.java
+++ src/main/java/com/camisola10/camisolabackend/adapter/persistence/product/ProductDb.java
@@ -8,6 +8,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.math.BigDecimal;
@@ -19,6 +20,8 @@ import java.util.List;
 @Builder
 @Data
 class ProductDb {
+    @Id
+    private String id;
     private String productId;
     private String name;
     private List<ProductCategory> categories;
