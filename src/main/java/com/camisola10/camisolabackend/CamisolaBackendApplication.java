7d1d700fce367ab0d30b5b3ce9f2866845a08eb5
joe7lopes <j.andre.oliveira@gmail.com>
2020-07-01 13:48:57 +0200

    set system default timezone to lisbon

--- src/main/java/com/camisola10/camisolabackend/CamisolaBackendApplication.java
+++ src/main/java/com/camisola10/camisolabackend/CamisolaBackendApplication.java
@@ -6,9 +6,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+import javax.annotation.PostConstruct;
+import java.time.ZoneId;
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableConfigurationProperties({S3Properties.class, JwtProperties.class})
 public class CamisolaBackendApplication {
+
+    @PostConstruct
+    public void init(){
+        TimeZone.setDefault(TimeZone.getTimeZone(ZoneId.of("Europe/Lisbon")));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(CamisolaBackendApplication.class, args);
