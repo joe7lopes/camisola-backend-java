b59be1f595c0204949c0ea1384e4ad805f6e98a2
joe7lopes <j.andre.oliveira@gmail.com>
2020-08-17 12:39:46 +0200

    added jenkins file

---
+++ jenkinsfile.groovy
@@ -0,0 +1,29 @@
+pipeline {
+    
+    stages {
+        stage('Checkout') {
+            steps {
+                checkout scm
+            }
+        }
+        stage('Unit & Integration Tests') {
+            steps {
+                script {
+                    try {
+                        sh './gradlew clean test --no-daemon' //run a gradle task
+                    } finally {
+                        junit '**/build/test-results/test/*.xml' //make the junit test results available in any case (success & failure)
+                    }
+                }
+            }
+        }
+        // stage('Publish Artifact to Nexus') {
+        //     steps {
+        //         sh './gradlew publish --no-daemon'
+        //     }
+        // }
+    }
+    post {
+        echo "finito"
+    }
+}
