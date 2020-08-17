de6f95f8d8d755183f8ad5db92a01ea33afc5b77
joe7lopes <j.andre.oliveira@gmail.com>
2020-08-17 12:55:44 +0200

    added jenkins file

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -17,6 +17,7 @@ pipeline {
                 }
             }
         }
+        
         // stage('Publish Artifact to Nexus') {
         //     steps {
         //         sh './gradlew publish --no-daemon'
